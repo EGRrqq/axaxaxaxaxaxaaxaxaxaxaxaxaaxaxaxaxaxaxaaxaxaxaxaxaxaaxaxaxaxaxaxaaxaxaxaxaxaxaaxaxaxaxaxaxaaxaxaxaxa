e06291a5dc41816ab913cc893d01f35a8b98b0d6
EGRrqq <egrrqqdev@gmail.com>
2024-04-21 14:15:56 -0400

    feat: decodeFields func

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,35 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+// console.log("Let's rock")
+// console.log({ encoded, translations })
+
+function decodeFields(encoded, translations) {
+  const excludeFields = ['groupId', 'service', 'formatSize', 'ca'];
+  const uniqIds = new Set();
+
+  const decoded = encoded.map((i) => {
+    const newI = { ...i };
+
+    for (let key in i) {
+      if (key.endsWith("Id") & !excludeFields.includes(key)) {
+        newI[key] = translations[i[key]];
+        uniqIds.add(i[key]);
+      }
+    }
+
+    return newI;
+  })
+
+  return { decoded, uniqIds }
+}
 
 
+const { decoded, uniqIds } = decodeFields(encoded, translations);
 
-// console.log(decoded)
+// logs
+console.log('Уникальные id:', {
+  my: Array.from(uniqIds).sort().filter(i => i),
+  translations: Object.keys(translations).sort()
+});
+
+console.log("Decoded: ", decoded)
