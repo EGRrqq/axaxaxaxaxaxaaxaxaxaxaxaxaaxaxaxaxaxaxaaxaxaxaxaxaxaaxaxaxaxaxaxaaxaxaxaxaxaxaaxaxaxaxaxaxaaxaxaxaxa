1eafaa270458c9aaa937baa45b054fa2dcf7bb6a
EGRrqq <egrrqqdev@gmail.com>
2024-04-22 04:37:34 -0400

    feat: base html markup

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,7 +1,47 @@
 <script lang="ts">
 </script>
 
-<main></main>
+<main>
+  <!-- Currency component -->
+  <article>
+    <section>
+      <div>
+        <input type="text" list="currency" autocomplete="off" />
+        <datalist id="currency">
+          <option value="eur">eur</option>
+          <option value="usd">usd</option>
+        </datalist>
+      </div>
+
+      <input type="number" value="0" />
+    </section>
+    <section></section>
+  </article>
+
+  <!-- Currency component -->
+  <article>
+    <section>
+      <div>
+        <input type="text" list="currency" autocomplete="off" />
+        <datalist id="currency">
+          <option value="eur">eur</option>
+          <option value="usd">usd</option>
+        </datalist>
+      </div>
+
+      <input type="number" value="0" />
+    </section>
+    <section></section>
+  </article>
+</main>
 
 <style>
+  article {
+    border: solid;
+    padding: 1rem;
+  }
+
+  main {
+    display: flex;
+  }
 </style>
